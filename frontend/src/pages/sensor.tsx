bab701694d2e84d7023b598007e58a38dd5b130e
Piotr <piotr@matchx.io>
2023-09-29 13:14:01 +0200

    Added listening to new blocks and parsing the device data details from the transaction data.

--- frontend/src/pages/sensor.tsx
+++ frontend/src/pages/sensor.tsx
@@ -4,9 +4,11 @@ import Layout from '@components/Layout'
 import { ReactElement, useState } from 'react'
 import ERC20_FACTORY, { FACTORY_ABI_FULL } from '@utils/contracts/erc20factory'
 import { ERC20_IMPL_ABI_FULL } from '@utils/contracts/erc20'
-import { useAccount, useContractRead, useProvider, useToken } from 'wagmi'
+import { useAccount, useContractRead, useProvider, useToken, useTransaction } from 'wagmi'
+import { watchBlockNumber } from '@wagmi/core'
 import { ethers } from 'ethers'
-import React from 'react'
+import React, { useEffect } from 'react'
+import {hexToBytes, bytesToUtf8, extractJSON} from '@utils/jsonparser'
 
 // type tokenInfo = {
 //   address: `0x${string}`
@@ -14,9 +16,8 @@ import React from 'react'
 //   symbol: string
 //   supply: string
 // }
-//const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
-
-
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+//var deviceInfo = "No Data";
 const GetTokenInfo = (tokenAddress: `0x${string}`): React.ReactElement => {
     const { data, isError, isLoading } = useToken({
       address: tokenAddress.token,
@@ -33,6 +34,15 @@ const GetTokenInfo = (tokenAddress: `0x${string}`): React.ReactElement => {
     </div>
     )
   }
+const GetTransaction = (): React.ReactElement => {
+    const { data, isError, isLoading } = useTransaction({
+      hash: '0x8bc804795c699794962a1dc189176dca985b9fb98bc4cac655300863ae8b51b0',
+    })
+   
+    if (isLoading) return <div>Fetching transaction…</div>
+    if (isError) return <div>Error fetching transaction</div>
+    return <div>Transaction: {JSON.stringify(data)}</div>
+  }
 
 // Retrieve tokens deployed by a user
 const Page: NextPageWithLayout = () => {
@@ -44,9 +54,40 @@ const Page: NextPageWithLayout = () => {
     functionName: `getERC20s`,
     args: [ethers.utils.getAddress(address || `0x`)],
   })
-  const tokenAddress : `0x${string}` = data[0];
-  
-  //console.log(data);
+
+  const [deviceName, setDeviceName] = React.useState("No Data");
+  const [deviceEui, setDeviceEui] = React.useState("No Data");
+  const [deviceData, setDeviceData] = React.useState("No Data");
+  const [currentBlock, setCurrentBlock] = React.useState("No Data");
+
+  const GetBlockNumberFunc = (): React.ReactElement => {
+    const publicClient = useProvider();
+    publicClient.on('block', (blockNumber) => {
+        setCurrentBlock(blockNumber);
+        let block = publicClient.getBlockWithTransactions(blockNumber);
+        block.then(function(x)
+            {
+                console.log(blockNumber)
+                for (let tx of x.transactions) {
+                    if (tx.from && tx.from.toLowerCase() === '0x452403368683016c54dd16871622648f37aa946c') {
+                        console.log("Transaction Found:", tx);
+                        //parseTransaction(tx);
+                        let utf8Str = bytesToUtf8(hexToBytes(tx.data));
+                        //console.log(jsonString);
+                        let jsonObj = JSON.parse(extractJSON(utf8Str));		
+                        console.log(jsonObj);
+                        setDeviceName(jsonObj.deviceInfo.deviceName);
+                        setDeviceEui(jsonObj.deviceInfo.devEui);
+                        setDeviceData(jsonObj.data);
+                        console.log(deviceName);
+                    }}
+            })
+    });
+    //console.log("get Block");
+    //console.log(block);
+    return (<div>{deviceName}</div>);
+  }
+
 
 const copyToClipboard = (text) => {
   navigator.clipboard.writeText(text)
@@ -56,22 +97,30 @@ const copyToClipboard = (text) => {
     .catch(err => {
       console.error('Error in copying text: ', err);
     });
-};
+    };
 
-//const {tokenInfo} = GetTokenInfo({tokenAddress});
-
+  GetBlockNumberFunc();
 
   return (
     <>
-      <div className="mx-auto w-3/5">
+
+    <div className="mx-auto w-3/5">
         <Card>
           <h5 className="text-2xl mb-4 font-bold tracking-tight text-sky-800 dark:text-white">
-            Your Tokens
+           Current Block Nuber
           </h5>
-          {isLoading && <div>Loading...</div>}
-          {!isLoading && data && (
+          <div className="text-4xl font-mono leading-8 text-emerald-600"> {currentBlock} </div>
+        </Card>
+    </div>    
+    <div className="mx-auto w-3/5">
+        <Card>
+            <h5 className="text-2xl mb-4 font-bold tracking-tight text-sky-800 dark:text-white">
+            Your Tokens
+            </h5>
+            {isLoading && <div>Loading...</div>}
+            {!isLoading && data && (
             <ul className="space-y-4 list-outside">
-              {data.map((token: `0x${string}`, index: number) => (
+                {data.map((token: `0x${string}`, index: number) => (
                 <li key={index} className="flex items-center list-outside">
                     <div className="mx-auto w-full">
                     <p className="text-1xl font-semibold leading-8 text-sky-800"> Token Contract Address: </p>
@@ -79,12 +128,23 @@ const copyToClipboard = (text) => {
                         {GetTokenInfo({token})}
                     </div>                                          
                 </li>
-              ))}
+                ))}
             </ul>
-          )}
-          
+            )}          
         </Card>
-      </div>
+    </div>
+    <div className="mx-auto w-3/5 break-words">
+            <Card> 
+            <h5 className="text-2xl mb-4 font-bold tracking-tight text-sky-800 dark:text-white">
+                Sensors Data
+            </h5>
+            <div>
+                <p className="text-1xl font-semibold leading-8 text-sky-800"> Device Name: </p> {deviceName}
+                <p className="text-1xl font-semibold leading-8 text-sky-800"> Device EUI: </p> {deviceEui} 
+                <p className="text-1xl font-semibold leading-8 text-sky-800"> Device Data: </p> {deviceData}
+            </div>
+        </Card>
+    </div>
     </>
   )
 }
---
+++ frontend/src/utils/jsonparser.ts
@@ -0,0 +1,38 @@
+// jsonParser.ts
+
+// Convert hex to byte array
+export function hexToBytes(hex: string): number[] {
+    const bytes: number[] = [];
+    for (let i = 2; i < hex.length; i += 2) {
+      bytes.push(parseInt(hex.substr(i, 2), 16));
+    }
+    return bytes;
+  }
+  
+  // Convert byte array to UTF-8 string
+  export function bytesToUtf8(byteArray: number[]): string {
+    let utf8 = "";
+    for (let i = 0; i < byteArray.length; i++) {
+      utf8 += String.fromCharCode(byteArray[i]);
+    }
+    return utf8;
+  }
+  
+  // Try to extract JSON from the string
+  export function extractJSON(str: string): string | null {
+    const leftBrace = str.indexOf('{');
+    const rightBrace = str.lastIndexOf('}');
+    if (leftBrace !== -1 && rightBrace !== -1) {
+      return str.substring(leftBrace, rightBrace + 1);
+    }
+    return null;
+  }
+  
+  // Export the functions as an object
+  const jsonParser = {
+    hexToBytes,
+    bytesToUtf8,
+    extractJSON,
+  };
+  
+  export default jsonParser;
