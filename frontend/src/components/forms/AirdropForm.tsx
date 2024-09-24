06fb2a1683705e9b98b051e7fc3ac84d7ef56219
Sheen Huth <9674168+sheenhx@users.noreply.github.com>
2024-09-24 12:14:24 +0200

    Update AirdropForm.tsx

--- frontend/src/components/forms/AirdropForm.tsx
+++ frontend/src/components/forms/AirdropForm.tsx
@@ -106,8 +106,9 @@ const AirdropForm = (): React.ReactElement<any, any> => {
             disabled={isAirdropComplete || isAirdropResultLoading}
             onClick={(e) => {
               console.log(`hello`)
+              console.log('Type of airdropTokensWrite:', typeof airdropTokensWrite)
               e.preventDefault()
-              airdropTokensWrite?.()
+              airdropTokensWrite.()
             }}
             type="submit"
           >
